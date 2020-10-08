fb3c8dcdc3791b11ab103a8d3e52c96d0998902b
Enoch2k2 <enoch2k2@hotmail.com>
2020-10-08 11:03:07 -0500

    Added characters reducer

---
+++ src/reducers/charactersReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    loading: true,
+    characters: []
+}
+
+export default (state=initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
